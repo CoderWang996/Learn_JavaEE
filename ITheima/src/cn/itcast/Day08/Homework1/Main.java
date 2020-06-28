faa7492fc9ddb14ec7220c102e837cba067d6687
Coder Wang <1351093262@qq.com>
2020-06-28 16:48:59 +0800

    JavaSE_Study

--- ITheima/src/cn/itcast/Day08/Homework1/Main.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.Day08.Homework1;
-//使用代码计算得出当前系统时间距离时间原点“1970年1月1日0时0分0秒”过了多少年，要求结果向下取整。
-public class Main {
-    public static void main(String[] args) {
-        long n=System.currentTimeMillis();
-        System.out.println(n/1000/60/60/24/365+"年");
-    }
-}
--- ITheima/src/cn/itcast/Day14/IO1/FileOutPutStremDemo2.java
+++
@@ -1,6 +0,0 @@
-package cn.itcast.Day14.IO1;
-
-public class FileOutPutStremDemo2 {
-    public static void main(String[] args) {
-    }
-}
