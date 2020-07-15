3a3ee3766ad88ace8ca96499256dfed67a58b990
Coder_WangChneyang <1351093262@qq.com>
2020-07-15 20:56:11 +0800

    JavaWeb核心_Day01

---
+++ Web_Day01/src/Servlet/Tell.java
@@ -0,0 +1,31 @@
+package Servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Tell  implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("这是俺的第一个servlet入门案例！");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
