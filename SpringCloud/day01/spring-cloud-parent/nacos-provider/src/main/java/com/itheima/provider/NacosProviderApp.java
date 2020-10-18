c26d42ba050d379a5b196303120ffe35f55e3f6c
Coder_WangChneyang <1351093262@qq.com>
2020-10-18 19:04:43 +0800

    SpringCloud

---
+++ SpringCloud/day01/spring-cloud-parent/nacos-provider/src/main/java/com/itheima/provider/NacosProviderApp.java
@@ -0,0 +1,11 @@
+package com.itheima.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosProviderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApp.class,args);
+    }
+}
