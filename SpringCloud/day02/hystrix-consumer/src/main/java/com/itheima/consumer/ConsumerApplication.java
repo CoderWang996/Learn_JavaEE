b742cb3e954e49bdf1c67c27b7893c4aea10bbe1
Coder_WangChneyang <1351093262@qq.com>
2020-10-22 21:24:26 +0800

    SpringCloud

--- SpringCloud/day02/hystrix-consumer/src/main/java/com/itheima/consumer/ConsumerApplication.java
+++ SpringCloud/day02/hystrix-consumer/src/main/java/com/itheima/consumer/ConsumerApplication.java
@@ -1,16 +1,30 @@
 package com.itheima.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EntityScan("com.itheima.consumer.domain")
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ConsumerApplication {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
