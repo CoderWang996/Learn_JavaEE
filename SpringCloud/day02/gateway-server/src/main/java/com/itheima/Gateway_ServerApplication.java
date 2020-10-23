24e154ba1cda0578eb9d1b8f956f9eb3c61468cf
Coder_WangChneyang <1351093262@qq.com>
2020-10-23 23:34:29 +0800

    SpringCloud

---
+++ SpringCloud/day02/gateway-server/src/main/java/com/itheima/Gateway_ServerApplication.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Gateway_ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway_ServerApplication.class,args);
+    }
+}
